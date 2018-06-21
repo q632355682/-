eb43b2960922545961226b67f723ba5dcbf380c3
lfalex <lfalex0831@163.com>
2018-06-21 11:39:11 +0800

    配置中心客户端

--- config-client/src/main/java/com/cn/controller/ConfigController.java
+++ config-client/src/main/java/com/cn/controller/ConfigController.java
@@ -3,6 +3,7 @@ package com.cn.controller;/**
  */
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @create: 2018-06-20 22:58
  **/
 @RestController
+@RefreshScope       //非重启项目手动刷新配置注解
 public class ConfigController {
 
     @Value("${version}")
