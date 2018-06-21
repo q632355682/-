bd826fe8f99b372b67dfe15bb9c8464a299ab425
lfalex <lfalex0831@163.com>
2018-06-21 11:40:28 +0800

    Config Bus

---
+++ config-bus-client/src/main/java/com/cn/ConfigBusApplication.java
@@ -0,0 +1,19 @@
+package com.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: springcloud-example
+ * @description:
+ * @author:
+ * @create: 2018-06-21 10:27
+ **/
+@SpringBootApplication
+public class ConfigBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigBusApplication.class, args);
+    }
+
+}
